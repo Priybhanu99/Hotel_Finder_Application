8da4a2dd1b0b6394df7afc100c74a9c366cf9717
Priybhanu Yadav <bhanuyadav1999.by@gmail.com>
2020-04-30 23:58:43 +0530

    Linking details to the database

--- app/src/main/java/com/example/hotelfinder/DatabaseHelper.java
+++ app/src/main/java/com/example/hotelfinder/DatabaseHelper.java
@@ -12,12 +12,22 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "hotels.db";
+
     public static final String TABLE_NAME = "room_table";
     public static final String COL_1 = "room_id";
     public static final String COL_2 = "name";
     public static final String COL_3 = "type";
     public static final String COL_4 = "location";
     public static final String COL_5 = "status";
+
+    public static final String DESCRIPTION = "description";
+    public static final String DES_col1 = "room_type";
+    public static final String DES_col2 = "no_of_beds";
+    public static final String DES_col3 = "no_of_washrooms";
+    public static final String DES_col4 = "service_charge";
+    public static final String DES_col5 = "wifi";
+
+
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -49,6 +59,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
 
+
+    }
+
+    public boolean add_data_for_roomdescription(String type,int beds,int washrooms,int service,String wifi){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DES_col1, type);
+        contentValues.put(DES_col2, beds);
+        contentValues.put(DES_col3, washrooms);
+        contentValues.put(DES_col4, service);
+        contentValues.put(DES_col5, wifi);
+        long result = db.insert(DESCRIPTION, null, contentValues);
+        if(result==-1)
+            return false;
+        else
+            return true;
 
     }
 
--- app/src/main/java/com/example/hotelfinder/Details.java
+++ app/src/main/java/com/example/hotelfinder/Details.java
@@ -3,17 +3,51 @@ package com.example.hotelfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Details extends AppCompatActivity {
 
+    DatabaseHelper myDb;
+    EditText e1,e2,e3,e4,e5;
+    Button addthis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        TextView t1 = (TextView)findViewById(R.id.heading);
+        EditText t1 = (EditText) findViewById(R.id.heading);
         Bundle b1 = getIntent().getExtras();
         String str = b1.getString("user");
         t1.setText(str);
+        e1 = t1;
+        e2 =(EditText)findViewById(R.id.beds);
+        e3 = (EditText)findViewById(R.id.washrooms);
+        e4 = (EditText)findViewById(R.id.wifi);
+        addthis = (Button)findViewById(R.id.addthis);
+        AddData();
+    }
+
+    private void AddData() {
+        addthis.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.add_data_for_roomdescription(e1.getText().toString(),
+                                Integer.parseInt(e2.getText().toString()),
+                                Integer.parseInt(e3.getText().toString()),
+                                Integer.parseInt(e4.getText().toString()),
+                                e5.getText().toString());
+                        if(isInserted == true)
+                            Toast.makeText(Details.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(Details.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
